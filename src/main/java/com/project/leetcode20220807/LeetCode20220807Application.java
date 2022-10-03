9ee32ea9798ccb826cc2788866a8d7683b1f11a3
Emerson Millard <emillard.code@gmail.com>
2022-10-03 16:47:03 -0700

    Initial commit

---
+++ src/main/java/com/project/leetcode20220807/LeetCode20220807Application.java
@@ -0,0 +1,13 @@
+package com.project.leetcode20220807;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCode20220807Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCode20220807Application.class, args);
+    }
+
+}
